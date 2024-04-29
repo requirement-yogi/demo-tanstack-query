3735e70598f3035100caae5f773768f05e2b479f
Corentin Briand <cbriand@r-yogi.com>
2024-04-29 21:00:46 +0200

    Update readme

--- README.md
+++ README.md
@@ -2,35 +2,54 @@ This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next
 
 ## Getting Started
 
-First, run the development server:
+First, install dependencies
 
 ```bash
 npm run dev
-# or
-yarn dev
-# or
-pnpm dev
-# or
-bun dev
+```
+
+Then set up your environment variables in a `.env.local` file in the root of the project. You can use the `.env.example` file as a template.
+
+```bash
+cp .env.example .env.local
+```
+
+Then, run the development server:
+
+```bash
+npm run dev
 ```
 
 Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
 
-You can start editing the page by modifying `app/page.tsx`. The page auto-updates as you edit the file.
+## Description
+This is a comparison of the way to make requests to an API, using a simple mock of a blog as an example.
 
-This project uses [`next/font`](https://nextjs.org/docs/basic-features/font-optimization) to automatically optimize and load Inter, a custom Google Font.
+ - By managing the state using the regular useEffect hooks provided by React
+ - By using the tanstack/react-query library
 
-## Learn More
+### Pages: 
+    - `/` - Home page
+    - `/vanilla` - Pages that uses the regular useEffect hooks
+    - `/tanstack` - Pages that uses the react-query library
 
-To learn more about Next.js, take a look at the following resources:
+Most differences are seen with the browser's network tab open, to see the number of requests made.
 
--   [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
--   [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
+### Server config
+You can configure the server to add a delay, or to randomly fail requests on the `/config` page, or in your `.env.local` file.
+Only the requests to the /post/... endpoints are affected by the delay and failure rate.
 
-You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js/) - your feedback and contributions are welcome!
+### Simulate other users
+You can simulate other users creating posts by running the following command in a separate terminal window:
 
-## Deploy on Vercel
+```bash
+# Creates a new random post every 5 seconds
+npm run simulate
+```
 
-The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
+### Branches
+- `main` - The initial state of the project
+- `auto-refetch` - Demonstrate the auto-refetch feature of react-query. The post timeline is updated regularly in the background.
+- `infinite-scrolling` - Remove the pagination, and simply scroll to the bottom of the page to load more posts.
+- `prefetch` - Post details are loaded in the background when hovering over a post "Read more" link.
 
-Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,75 @@
+import {ReactNode} from "react";
+
 /**
  * Home page for the application
  */
 export default function Home() {
-    return <div>Presentation</div>;
+    return (
+        <div className={"max-w-prose text-gray-200 my-2 mx-auto"}>
+            <Title>Site description</Title>
+            <Paragraph>
+                This is a comparison of the way to make requests to an API, using a simple mock of a blog as an example.
+            </Paragraph>
+            <List>
+                <ListItem>By managing the state using the regular useEffect hooks provided by React</ListItem>
+                <ListItem>By using the tanstack/react-query library</ListItem>
+            </List>
+            <SubTitle>Pages:</SubTitle>
+            <List>
+                <ListItem>`/vanilla/...` - Pages that uses the regular useEffect hooks</ListItem>
+                <ListItem>`/tanstack/...` - Pages that uses the react-query library</ListItem>
+                <ListItem>`/config` - Server config</ListItem>
+            </List>
+            <Paragraph>
+                Most differences are seen with the browser's network tab open, to see the number of requests made.
+            </Paragraph>
+            <SubTitle>Server config</SubTitle>
+            <Paragraph>
+                You can configure the server to add a delay, or to randomly fail requests on the `/config` page, or in
+                your `.env.local` file. Only the requests to the /post/... endpoints are affected by the delay and
+                failure rate.
+            </Paragraph>
+            <SubTitle>Simulate other users</SubTitle>
+            <Paragraph>
+                You can simulate other users creating posts by running the following command in a separate terminal
+                window:
+            </Paragraph>
+            <code className={"border-1 bg-opacity-10 bg-green-400 p-2 rounded m-2   "}>npm run simulate</code>
+            <SubTitle>Git branches</SubTitle>
+            <List>
+                <ListItem>`main` - The initial state of the project</ListItem>
+                <ListItem>
+                    `auto-refetch` - Demonstrate the auto-refetch feature of react-query. The post timeline is updated
+                    regularly in the background.
+                </ListItem>
+                <ListItem>
+                    `infinite-scrolling` - Remove the pagination, and simply scroll to the bottom of the page to load
+                    more posts.
+                </ListItem>
+                <ListItem>
+                    `prefetch` - Post details are loaded in the background when hovering over a post "Read more" link.
+                </ListItem>
+            </List>
+        </div>
+    );
 }
+
+const Title = ({ children }: { children: ReactNode }) => {
+    return <h1 className="text-2xl text-amber-300 my-5">{children}</h1>;
+};
+
+const Paragraph = ({ children }: { children: ReactNode }) => {
+    return <p className=" my-2">{children}</p>;
+};
+
+const SubTitle = ({ children }: { children: ReactNode }) => {
+    return <h2 className="text-xl text-amber-200 mt-6 mb-2">{children}</h2>;
+};
+
+const ListItem = ({ children }: { children: ReactNode }) => {
+    return <li className="my-2">{children}</li>;
+};
+
+const List = ({ children }: { children: ReactNode }) => {
+    return <ul className={"list-disc ml-4"}>{children}</ul>;
+};
